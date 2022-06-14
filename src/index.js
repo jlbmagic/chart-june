daf33555056ddb86a8414d90a70663419507b240
Jeremy Brown <jlbmagic@gmail.com>
2022-06-14 13:42:30 -0400

    added configs from FM and set up onclick

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import c3 from "c3";
 
-var chart = c3.generate({
-  bindto: "#chart",
-  size: { width: 1000, height: 700 },
-  data: {
-    colors: {
-      Apples: "red",
-      Peaches: "green",
-    },
-    type: "bar",
-    columns: [
-      ["Apples", 300, 200, 100, 400, 350, 250],
-      ["Peaches", 10, 60, 210, 340, 415, 225],
-    ],
-  },
-});
+window.loadChart = function (param) {
+  const obj = JSON.parse(param);
+  const json = obj.data;
+  // const type = obj.type;
+  // const height = obj.height || 900;
+  // const angle = obj.angle || 0;
+
+  const { type, height, angle } = obj;
+
+  var chart = c3.generate({
+    bindto: "#chart",
+    size: { height: height },
+    axis: { x: { type: "category" } },
+    pie: { padAngle: angle },
+    data: {
+      onclick: function (d) {
+        console.log(d);
+
+        FileMaker.PerformScript("Get Data from JS", JSON.stringify(d));
+      },
+      colors: {
+        apples: "#F241A3",
+        peaches: "#020659",
+        bananas: "#EF27F2"
+      },
+      type: type,
+      keys: { x: "month", value: ["apples", "peaches", "bananas"] },
+      json: json
+    }
+  });
+};
